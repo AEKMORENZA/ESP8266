4d6b00797276acf242459b37d0413d06aa4c23c8
AEKMORENZA <42500428+AEKMORENZA@users.noreply.github.com>
2018-10-23 22:52:46 +0200

    Update PRUEBA_NODEMCU_DHT11/PRUEBA_NODEMCU_DHT11.ino

---
+++ PRUEBA_NODEMCU_DHT11/PRUEBA_NODEMCU_DHT11.ino
@@ -0,0 +1,130 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <DHT11.h>
+
+int pin = D2;
+unsigned long previousMillis = 0;
+unsigned long interval = 120000;
+const char* ssid     = "MIWIFI_UKka";      // SSID
+const char* password = "zhUsbRnKzDZK";      // Password
+const char* host = "192.168.1.128";  // IP serveur - Server IP
+const int   port = 5045;            // Port serveur - Server Port
+String cmd = "";
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+IPAddress timeServerIP; // time.google.com NTP server address
+const char* ntpServerName = "time.google.com";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+WiFiClient client;
+WiFiUDP udp;
+DHT11 dht11(pin);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
+}
+
+void loop()
+{
+  WiFi.hostByName(ntpServerName, timeServerIP);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+
+    previousMillis = currentMillis;
+
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    delay(1000);
+
+    int cb = udp.parsePacket();
+    if (!cb) {
+      Serial.println("no packet yet");
+    } else {
+      Serial.print("packet received, length=");
+      Serial.println(cb);
+      udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      const unsigned long seventyYears = 2208988800UL;
+      unsigned long epoch = secsSince1900 - seventyYears;
+      client.print(((epoch  % 86400L) / 3600) + 2); // print the hour (86400 equals secs per day)
+      client.print(':');
+      if (((epoch % 3600) / 60) < 10) {
+        client.print('0');
+      }
+      client.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+      client.print(':');
+      if ((epoch % 60) < 10) {
+        client.print('0');
+      }
+      client.print(epoch % 60); // print the second
+      client.print(", ");
+      client.flush();
+
+      readDHT11();
+
+    }
+  }
+}
+void readDHT11() {
+  int err;
+  float temp, hum;
+  if ((err = dht11.read(hum, temp)) == 0)   // Si devuelve 0 es que ha leido bien
+  {
+    client.connect(host, port);
+    Serial.println("conectado");
+    client.print("Temperatura: ");
+    client.print(temp);
+    client.print(", ");
+    client.print(" Humedad: ");
+    client.print(hum);
+    client.println();
+    client.flush();
+  }
+  else
+  {
+    client.connect(host, port);
+    Serial.println("error");
+    client.println();
+    client.print("Error Num :");
+    client.print(err);
+    client.println();
+    client.flush();
+  }
+}
+void sendNTPpacket(IPAddress & address) {
+  Serial.println("sending NTP packet...");
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  udp.beginPacket(address, 123);
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
