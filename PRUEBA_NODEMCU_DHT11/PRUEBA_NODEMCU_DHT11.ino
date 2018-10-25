621dcb14da1f9bc5a909c9555406dadf81b0ffd5
AEKMORENZA <42500428+AEKMORENZA@users.noreply.github.com>
2018-10-25 22:55:33 +0200

    Update PRUEBA_NODEMCU_DHT11.ino

--- PRUEBA_NODEMCU_DHT11/PRUEBA_NODEMCU_DHT11.ino
+++ PRUEBA_NODEMCU_DHT11/PRUEBA_NODEMCU_DHT11.ino
@@ -1,10 +1,18 @@
+/*Código que lee un sensor de humedad y temperatura y envia las lecturas sobre wifi a un socket TCP
+  además realiza consultas NTP periódicas para añadir un timestamp a las lecturas del sensor
+  placa de desarrollo Node-MCU V1.0 (ESP-12E)
+*/
+
+
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <DHT11.h>
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
 
 int pin = D2;
 unsigned long previousMillis = 0;
-unsigned long interval = 120000;
+unsigned long interval = 60000;
 const char* ssid     = "MIWIFI_UKka";      // SSID
 const char* password = "zhUsbRnKzDZK";      // Password
 const char* host = "192.168.1.128";  // IP serveur - Server IP
@@ -28,6 +36,52 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+  // Port defaults to 8266
+  ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -46,6 +100,8 @@ void setup() {
 
 void loop()
 {
+  ArduinoOTA.handle();
+
   WiFi.hostByName(ntpServerName, timeServerIP);
   unsigned long currentMillis = millis();
 
